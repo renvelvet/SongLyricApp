6c9468cc431a6f4e5ad478ea0b4d1f90a888517e
renvelvet <reshapuspita734@gmail.com>
2021-03-11 11:33:00 +0700

    done v1

--- app/src/main/java/com/dicoding/songlyricapp/SongLyricActivity.kt
+++ app/src/main/java/com/dicoding/songlyricapp/SongLyricActivity.kt
@@ -1,29 +1,57 @@
 package com.dicoding.songlyricapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.icu.text.CaseMap
+import android.net.Uri
 import android.os.Bundle
-import android.view.CollapsibleActionView
-import android.view.TextureView
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import com.google.android.material.appbar.CollapsingToolbarLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
-class SongLyricActivity : AppCompatActivity() {
+
+class SongLyricActivity : AppCompatActivity(), View.OnClickListener {
 
     companion object {
         const val KEY_SONG = "key_song"
     }
 
+    lateinit var dataSong : Song
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_song_lyric)
-//        setSupportActionBar(findViewById(R.id.toolbar))
+        dataSong = intent.getParcelableExtra<Song>(KEY_SONG) as Song
 
-        val dataSong = intent.getParcelableExtra<Song>(KEY_SONG) as Song
-//        findViewById<CollapsingToolbarLayout>()
         findViewById<TextView>(R.id.title).text = dataSong.title
         findViewById<TextView>(R.id.lyric).text = dataSong.lyric
         findViewById<TextView>(R.id.album).text = dataSong.album
         findViewById<ImageView>(R.id.thumbnail).setImageResource(dataSong.thumbnail)
+
+        val favoriteBtn = findViewById(R.id.favorite) as ImageView
+        favoriteBtn.setOnClickListener(this)
+
+        val shareBtn = findViewById(R.id.share_btn) as ImageView
+        shareBtn.setOnClickListener(this)
     }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.favorite -> {
+                Toast.makeText(this,"Added to Lovelist!", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.share_btn -> {
+                val tweet = "Now playing:"
+                val song = dataSong.title.toString()
+                val tweetUrl = ("https://twitter.com/intent/tweet?text=$tweet &url="
+                        + "\"$song\" by Sasha Sloan")
+                val uri: Uri = Uri.parse(tweetUrl)
+                startActivity(Intent(Intent.ACTION_VIEW, uri))
+            }
+        }
+    }
+
 }
